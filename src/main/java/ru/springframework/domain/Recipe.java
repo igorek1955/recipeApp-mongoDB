eee590a9a5741413ff03960f26f1cd2ead602a95
igorek1955 <igorek1955@gmaii.com>
2020-11-04 00:15:41 +0300

    bug fixing

--- src/main/java/ru/springframework/domain/Recipe.java
+++ src/main/java/ru/springframework/domain/Recipe.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 
 @Getter
@@ -15,8 +16,7 @@ import java.util.Set;
 @Document
 public class Recipe {
 
-    @Id
-    private String id;
+    private String id = UUID.randomUUID().toString();
     private String description;
     private Integer prepTime;
     private Integer cookTime;
